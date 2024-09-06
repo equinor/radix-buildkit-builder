80e57d1c2f9fa6eecf45ceee8631affa77774916
Nils Gustav Stråbø <nst@equinor.com>
2024-09-06 15:58:10 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM ghcr.io/jqlang/jq:1.7.1 AS jq
+
+FROM quay.io/buildah/stable:v1.31 AS buildah
+
+COPY --from=jq /jq /usr/local/bin/jq
+
+COPY --chmod=111 build.sh /usr/local/bin/build.sh
+
+ENTRYPOINT [ "build.sh" ]
---
+++ build.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+set -e -u -o pipefail
+
+push=0
+use_cache=0
+git_commit_hash=""
+git_tags=""
+target_environments=""
+secrets=()
+
+# https://www.atatus.com/blog/bash-scripting/
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --registry)
+      registry="$2"
+      shift
+      ;;
+    --registry-username)
+      registry_username="$2"
+      shift
+      ;;
+    --registry-password)
+      registry_password="$2"
+      shift
+      ;;
+    --use-cache)
+      use_cache=1
+      ;;
+    --cache-registry)
+      cache_registry="$2"
+      shift
+      ;;
+    --cache-registry-username)
+      cache_registry_username="$2"
+      shift
+      ;;
+    --cache-registry-password)
+      cache_registry_password="$2"
+      shift
+      ;;
+    --cache-repository)
+      cache_repository="$2"
+      shift
+      ;;
+    --branch)
+      branch="$2"
+      shift
+      ;;
+    --tag)
+      tag="$2"
+      shift
+      ;;
+    --cluster-type-tag)
+      cluster_type_tag="$2"
+      shift
+      ;;
+    --cluster-name-tag)
+      cluster_name_tag="$2"
+      shift
+      ;;
+    --push)
+      push=1
+      ;;
+    --git-commit-hash)
+      git_commit_hash="$2"
+      shift
+      ;;
+    --git-tags)
+      git_tags="$2"
+      shift
+      ;;
+    --target-environments)
+      target_environments="$2"
+      shift
+      ;;
+    --secrets)
+      secrets+=(${2//,/ })
+      shift
+      ;;
+    --secrets-path)
+      secrets_path="$2"
+      shift
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+# https://unix.stackexchange.com/questions/444946/how-can-we-run-a-command-stored-in-a-variable
+
+echo buildah login \
+    --username "${registry_username}" \
+    --password "${registry_password}" \
+    ${registry}
+
+if [[ $use_cache -eq 1 ]]; then
+    echo buildah login \
+        --user-name "${cache_registry_username}" \
+        --password "${cache_registry_password}" \
+        ${cache_registry}
+fi
+
+build_args=(
+    --storage-driver=overlay
+    --isolation=chroot
+    --jobs 0
+    --ulimit nofile=4096:4096
+    --build-arg RADIX_GIT_COMMIT_HASH="${git_commit_hash}"
+    --build-arg RADIX_GIT_TAGS="${git_tags}"
+    --build-arg BRANCH="${branch}"
+    --build-arg TARGET_ENVIRONMENTS="${target_environments}"
+)
+
+for s in "${secrets[@]}"
+do
+    build_args+=(
+        --secret id="$s",src="${secrets_path}"/"$s"
+    )
+done
+
+if [[ $use_cache -eq 1 ]]; then
+    build_args+=(
+        --layers
+        --cache-to="${cache_repository}"
+        --cache-from="${cache_repository}"
+    )
+fi
+
+if [[ $push -eq 1 ]]; then
+    build_args+=(
+        --tag "${tag}"
+        --tag "${cluster_type_tag}"
+        --tag "${cluster_name_tag}"
+    )
+fi
+
+# expand the array, run the command
+echo buildah build "${build_args[@]}"
+
+if [[ $push -eq 1 ]]; then
+    echo buildah push --storage-driver=overlay "${tag}"
+    echo buildah push --storage-driver=overlay "${cluster_type_tag}"
+    echo buildah push --storage-driver=overlay "${cluster_name_tag}"
+fi
